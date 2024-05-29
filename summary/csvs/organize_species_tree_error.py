676bd2f8bf994eed6b0eab52714191cf58410ccf
Erin  Molloy <ekmolloy@erins-mbp.myfiosgateway.com>
2024-05-28 22:52:39 -0400

    adding sanity check

--- summary/csvs/organize_species_tree_error.py
+++ summary/csvs/organize_species_tree_error.py
@@ -20,24 +20,31 @@ cols = ["MODL", "NRET", "MTHD", "THRC",
 rows = []
 
 for modl in ["5taxa", "6taxa", "26taxa", "Palaeognathae"]:
-	wdf = df[df["MODL"] == modl]
-
 	if (modl == "5taxa") or (modl == "6taxa"):
 		nrets = [100000]
 	else:
 		nrets = [10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000]
-
 	for nret in nrets:
-		xdf = wdf[wdf["NRET"] == nret]
 		for mthd in mthds:
-			ydf = xdf[xdf["MTHD"] == mthd]
+			xdf = df[(df["MODL"] == modl) & 
+				 (df["NRET"] == nret) & 
+				 (df["MTHD"] == mthd)]
 			
-			if ydf.shape[0] != 25:
-				print(ydf)
+			if xdf.shape[0] != 25:
+				print(xdf)
 				sys.exit("Missing replicates!\n")
 
-			fnr = ydf["FN"] / ydf["NINT_TRUE"]
-			fpr = ydf["FP"] / ydf["NINT_ESTI"]  # Correcting formula
+			for repl in range(1, 25):
+				ydf = xdf[(xdf["REPL"] == repl)]
+				nint_true = ydf["NINT_TRUE"].values[0]
+				nint_esti = ydf["NINT_ESTI"].values[0]
+				fn = ydf["FN"].values[0]
+				fp = ydf["FP"].values[0]
+				if nint_true - fn != nint_esti - fp:
+					sys.exit("Something bad happened!!!!!\n")
+
+			fnr = xdf["FN"] / xdf["NINT_TRUE"]
+			fpr = xdf["FP"] / xdf["NINT_ESTI"]  # Correcting formula
 
 			row = {}
 			row["MODL"] = modl
@@ -45,11 +52,11 @@ for modl in ["5taxa", "6taxa", "26taxa", "Palaeognathae"]:
 			row["MTHD"] = mthd
 			row["THRC"] = 0.0
 
-			row["AVG_FN"] = numpy.mean(ydf["FN"])
-			row["AVG_FP"] = numpy.mean(ydf["FP"])
+			row["AVG_FN"] = numpy.mean(xdf["FN"])
+			row["AVG_FP"] = numpy.mean(xdf["FP"])
 
-			row["STD_FN"] = numpy.std(ydf["FN"])
-			row["STD_FP"] = numpy.std(ydf["FP"])
+			row["STD_FN"] = numpy.std(xdf["FN"])
+			row["STD_FP"] = numpy.std(xdf["FP"])
 
 			row["SE_FN"] = row["STD_FN"] / numpy.sqrt(25)
 			row["SE_FP"] = row["STD_FP"] / numpy.sqrt(25)
--- summary/csvs/organize_species_tree_error_collapsed.py
+++ summary/csvs/organize_species_tree_error_collapsed.py
@@ -18,16 +18,25 @@ for modl in ["26taxa", "Palaeognathae"]:
 	for mthd in mthds:
 		for thrc in [0.5, 0.6, 0.7, 0.8, 0.9]:
 			for nret in [10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000]:
-				zdf = df[(df["NRET"] == nret) & 
+				xdf = df[(df["NRET"] == nret) & 
 				         (df["MTHD"] == mthd) &
 				         (df["MODL"] == modl) &
 				         (df["THRC"] == thrc)]				
 
-				if zdf.shape[0] != 25:
+				if xdf.shape[0] != 25:
 					sys.exit("Missing replicates!\n")
 
-				fnr = zdf["FN"] / zdf["NINT_TRUE"]
-				fpr = zdf["FP"] / zdf["NINT_ESTI"]  # Correcting formula
+				for repl in range(1, 25):
+					ydf = xdf[(xdf["REPL"] == repl)]
+					nint_true = ydf["NINT_TRUE"].values[0]
+					nint_esti = ydf["NINT_ESTI"].values[0]
+					fn = ydf["FN"].values[0]
+					fp = ydf["FP"].values[0]
+					if nint_true - fn != nint_esti - fp:
+						sys.exit("Something bad happened!!!!!\n")
+
+				fnr = xdf["FN"] / xdf["NINT_TRUE"]
+				fpr = xdf["FP"] / xdf["NINT_ESTI"]  # Correcting formula
 
 				#x = numpy.mean(zdf["FPR"])
 				#y = numpy.mean(fpr)
@@ -43,11 +52,11 @@ for modl in ["26taxa", "Palaeognathae"]:
 				row["MTHD"] = mthd
 				row["THRC"] = thrc
 
-				row["AVG_FN"] = numpy.mean(zdf["FN"])
-				row["AVG_FP"] = numpy.mean(zdf["FP"])
+				row["AVG_FN"] = numpy.mean(xdf["FN"])
+				row["AVG_FP"] = numpy.mean(xdf["FP"])
 
-				row["STD_FN"] = numpy.std(zdf["FN"])
-				row["STD_FP"] = numpy.std(zdf["FP"])
+				row["STD_FN"] = numpy.std(xdf["FN"])
+				row["STD_FP"] = numpy.std(xdf["FP"])
 
 				row["SE_FN"] = row["STD_FN"] / numpy.sqrt(25)
 				row["SE_FP"] = row["STD_FP"] / numpy.sqrt(25)
